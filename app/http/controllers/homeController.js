df832ae5e6a1f6ead95373a40e4a5d2ec1605b6f
Adil Ansari <adil101078@gmail.com>
2021-03-22 20:21:03 +0530

    All done

---
+++ app/http/controllers/homeController.js
@@ -0,0 +1,11 @@
+const Menu = require('../../models/menu')
+function homeController() {
+    return {
+        async index(req, res) {
+            const foods = await Menu.find()
+            return res.render('home', { foods: foods })
+        }
+    }
+}
+
+module.exports = homeController
